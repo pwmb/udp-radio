7619394efd7588d21045ec75ca3e20cf06107efa
Soumesh Banerjee <soumesh.banerjee@kony.com>
2017-12-28 13:15:12 +0530

    Fix some typos, and added to ignore self broadcast message

--- radio.js
+++ radio.js
@@ -2,17 +2,21 @@ const dgram = require('dgram');
 
 class Radio {
   constructor(broadcast_address, broadcast_port, onReceive) {
-    this.receiver = dgram.createSocket("udp4")
-    this.sender   = dgram.createSocket("udp4")
-    this.port     = broadcast_port
-    this.addr     = broadcast_address
+    this.sender     = dgram.createSocket("udp4")
+    this.receiver   = dgram.createSocket("udp4")
+    this.port       = broadcast_port
+    this.addr       = broadcast_address
 
-    this.sender.bind(this.port)
-    this.sender.on('error', (err) => {
+    this.receiver.bind(this.port)
+    this.receiver.on('error', (err) => {
       console.error(`[Radio Receiver Error] : Something went wrong!\n${err}`)
-      this.sender.close()
+      this.receiver.close()
     })
-    this.sender.on('message', (msg, rinfo) => {
+    this.receiver.on('message', (msg, rinfo) => {
+      if (this.sender.address().address == rinfo.address && this.sender.address().port == rinfo.port) {
+        // ignore self broadcast
+        return
+      }
       try {
         msg = JSON.parse(msg.toString())
       } catch (e) {
@@ -22,7 +26,7 @@ class Radio {
       }
     })
 
-    this.receiver.bind(function() {
+    this.sender.bind(function() {
       this.setBroadcast(true)
     })
   }
@@ -30,7 +34,7 @@ class Radio {
     if (typeof(message) === 'object') {
       // convert the object to buffer
       const buffer = new Buffer.from(JSON.stringify(message))
-      this.receiver.send(buffer, this.port, this.addr, (error, success)=>{
+      this.sender.send(buffer, this.port, this.addr, (error, success)=>{
         if (error != null) {
           console.error(`[Radio Broadcast Error] : Something went wrong!\n${error}`)
         }
